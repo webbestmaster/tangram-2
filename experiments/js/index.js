f014e760b09949baf4a22d3ca07c6919f13e1fac
dmitriy.turovtsov <web.best.master@gmail.com>
2017-01-19 22:45:00 +0300

    hackerman win!

--- experiments/js/index.js
+++ experiments/js/index.js
@@ -10,36 +10,4 @@ Try GreenSock's MorphSVGPlugin for free.
 
 */
 
-var tl = new TimelineLite({onUpdate:updateSlider}),
-    circle = document.getElementById("circle");
 
-tl.to(circle, 1, {morphSVG:"#hippo"}, "+=1")
-  .to(circle, 1, {morphSVG:"#star"}, "+=1")
-  .to(circle, 1, {morphSVG:"#elephant"}, "+=1")
-  .to(circle, 1, {morphSVG:circle}, "+=1");
-
-
-
-
-
-
-
-
-/* controls */
-
-$("#slider").slider({
-  range: false,
-  min: 0,
-  max: 1,
-  step:.001,
-  slide: function ( event, ui ) {
-    tl.progress( ui.value ).pause();
-  },
-  stop: function () {
-    tl.play();
-  }
-});	
-
-function updateSlider() {
-  $("#slider").slider("value", tl.progress());
-}
